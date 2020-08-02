8cab6b684197389dbc8cde03e944e6feedc78162
bharath3687 <69030760+bharath3687@users.noreply.github.com>
2020-08-02 23:44:14 +0530

    Login and Registeration jsp with controller
    updated

--- LoginController.java
+++ LoginController.java
@@ -1,50 +1,38 @@
-package Services;
+package s3.UserRegistrationAndLogin.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import Services.Login;
-import Services.User;
-
-
-@Controller  
-public class LoginController {  
-/*@RequestMapping("/logincall")  
-    public String display()  
-    {  
-        return "login";  
-    }*/   @Autowired
-    userService userService;
-
-	  @RequestMapping(value = "/login", method = RequestMethod.GET)
-	  public ModelAndView showLogin(HttpServletRequest request, HttpServletResponse response) {
-	    ModelAndView mav = new ModelAndView("login");
-	    mav.addObject("login", new Login());
-
-	    return mav;
-	  }
-	  @RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
-	  public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
-	      @ModelAttribute("login") Login login) {
-	    ModelAndView mav = null;
-
-	    User user = userService.validateUser(login);
-
-	    if (null != user) {
-	      mav = new ModelAndView("welcome");
-	      mav.addObject("firstname", user.getUserName());
-	    } else {
-	      mav = new ModelAndView("login");
-	      mav.addObject("message", "Username or Password is wrong!!");
-	    }
-
-	    return mav;
-	  }
-} 
+import s3.UserRegistrationAndLogin.model.Login;
+import s3.UserRegistrationAndLogin.model.User;
+import s3.UserRegistrationAndLogin.service.UserService;
+@Controller
+public class LoginController {
+  @Autowired
+  UserService userService;
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
+  public ModelAndView showLogin(HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView mav = new ModelAndView("login");
+    mav.addObject("login", new Login());
+    return mav;
+  }
+  @RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
+  public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+  @ModelAttribute("login") Login login) {
+    ModelAndView mav = null;
+    User user = userService.validateUser(login);
+    if (null != user) {
+    mav = new ModelAndView("welcome");
+    mav.addObject("firstname", user.getFirstname());
+    } else {
+    mav = new ModelAndView("login");
+    mav.addObject("message", "Username or Password is wrong!!");
+    }
+    return mav;
+  }
+}
--- RegistrationController.java
+++ RegistrationController.java
@@ -1,42 +1,29 @@
-package Services;
+package s3.UserRegistrationAndLogin.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import Services.User;
-
-
-  
-@Controller  
-public class RegistrationController { 
-	 @Autowired
-	  public userService userService;
-/*@RequestMapping("/registercall")  
-    public String display()  
-    {  
-        return "register";  
-    }     
-} */ 
-@RequestMapping(value = "/registercall", method = RequestMethod.GET)
-public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response) {
-  ModelAndView mav = new ModelAndView("register");
-  mav.addObject("user", new User());
-
-  return mav;
-}
-@RequestMapping(value = "/registerprocess", method = RequestMethod.POST)
-public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
-    @ModelAttribute("user") User user) {
-
+import s3.UserRegistrationAndLogin.model.User;
+import s3.UserRegistrationAndLogin.service.UserService;
+@Controller
+public class RegistrationController {
+  @Autowired
+  public UserService userService;
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView mav = new ModelAndView("register");
+    mav.addObject("user", new User());
+    return mav;
+  }
+  @RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
+  public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
+  @ModelAttribute("user") User user) {
   userService.register(user);
-
-  return new ModelAndView("welcome",  user.getUserName());
-}
+  return new ModelAndView("welcome", "firstname", user.getFirstname());
+  }
 }
