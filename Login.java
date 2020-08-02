6fa2a169a017c938540cef98fba96f105be959cc
Srinimaddy <66350330+Srinimaddy@users.noreply.github.com>
2020-08-02 23:15:31 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,21 @@
+package Services;
+
+public class Login {
+String Email,Password;
+
+public String getEmail() {
+	return Email;
+}
+
+public void setEmail(String email) {
+	Email = email;
+}
+
+public String getPassword() {
+	return Password;
+}
+
+public void setPassword(String password) {
+	Password = password;
+}
+}
